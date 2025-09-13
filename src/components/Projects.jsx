53ab2d54be59f4e9ecb02ffa60b8219611528309
durgayarabati <prasadyarabati16@gmail.com>
2025-09-13 15:31:32 +0530

    some changes in project  section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const projects = [
   },
   {
     title: 'Portfolio Website - React Js',
-    image: 'https://unsplash.com/photos/macbook-pro-on-black-table-t5SfH5KowBM',
+    image: 'https://images.unsplash.com/photo-1659885785824-3e72856b8fef?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     liveLink: 'https://your-live-link.com',
     githubLink: 'https://github.com/durgayarabati',
   },
